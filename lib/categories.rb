c84d45a6b14dc16a76afcabb35210a3dc86d1dda
Cesar Gavino <cesargavino@gmail.com>
2022-03-10 17:26:56 -0500

    Adition of structure of categories and transactions

--- lib/categories.rb
+++ lib/categories.rb
@@ -1,4 +1,20 @@
 require "date"
 class Categories
   def initialize; end
+
+  def create_cat; end
+
+  def show_cat; end
+
+  def update_cat; end
+
+  def delete_cat; end
+
+  def add_to_cat; end
+
+  def toggle_cat; end
+
+  def next_cat; end
+
+  def preview_cat; end
 end
--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,4 +1,16 @@
 require "date"
 class Transactions
   def initialize; end
+
+  def show_tr; end
+
+  def add_tr; end
+
+  def update_tr; end
+
+  def delete_tr; end
+
+  def next_tr; end
+
+  def preview_tr; end
 end
