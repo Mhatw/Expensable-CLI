56e1952c3a0addb020c65bb6a27eccd497f360c2
mhatw <contactomhatw@gmail.com>
2022-03-12 19:29:18 -0500

    added category and m_category changes

--- src/categories.rb
+++ src/categories.rb
@@ -1,19 +1,26 @@
 require "date"
 require_relative "services"
+require_relative "m_categories"
 require "terminal-table"
 
 class Categories
+  include Mcategories
   def initialize
     @cat = Services::Session.new
     @cat.login(credentials: { email: "test7@mail.com", password: "123456" })
     @index_cat = @cat.index_categories[:content]
     @month_var_cat = 5
     @toggle_value = "expense"
-    # pp @index_cat = @cat.index_categories[:content].select { |n| n[:transaction_type] == "expense"}
-    show_cat(@toggle_value)
+    menu
   end
 
-  def create_cat; end
+  # new_category: { name: string, transaction_type: expense }
+  def create_cat
+    new_cat = cat_get_info
+    new_cat_on = @cat.create_category(new_category: new_cat)
+    @index_cat << new_cat_on[:content]
+    show_cat(@toggle_value)
+  end
 
   def show_cat(type)
     @date_today = Date.today << @month_var_cat # trae fecha actual y le suma o resta dependiendo
@@ -31,11 +38,27 @@ class Categories
     puts table
   end
 
-  def update_cat; end
+  def update_cat(id)
+    upd_cat = cat_get_info
+    @cat.update_category(category_id: id, new_data: upd_cat)
+    (@index_cat.select { |n| n[:id] == id.to_i })[0][:name] = upd_cat[:name]
+    (@index_cat.select { |n| n[:id] == id.to_i })[0][:transaction_type] = upd_cat[:transaction_type]
+    show_cat(@toggle_value)
+  end
 
-  def delete_cat; end
+  def delete_cat(id)
+    @cat.delete_category(category_id: id)
+    @index_cat.delete_if { |dd| dd[:id] == id }
+    show_cat(@toggle_value)
+  end
 
-  def add_to_cat; end
+  def add_to_cat(id)
+    new_trs = add_to_info
+    new_trs_on = @cat.create_transaction(category_id: id, new_data: new_trs)
+    pp new_trs_on
+    (@index_cat.select { |n| n[:id] == id.to_i })[0][:transactions] << new_trs_on[:content]
+    show_cat(@toggle_value)
+  end
 
   def toggle_cat
     @toggle_value = @toggle_value == "expense" ? "income" : "expense"
@@ -52,25 +75,18 @@ class Categories
     show_cat(@toggle_value) # traigo la tabla denuevo pero se actualiza el month_var_cat y retrocedo
   end
 
-  def menu_prov
+  def menu
+    show_cat(@toggle_value)
     action = ""
-    until action == "exit"
-      puts "next | prev | show | toggle"
+    until action == "logout"
+      puts "create | show ID | update ID | delete ID\nadd-to ID | toggle | next | prev | logout"
+      print "> "
       action = gets.chomp
-      case action
-      when "show"
-        show_cat(@toggle_value)
-      when "next"
-        next_cat
-      when "prev"
-        preview_cat
-      when "toggle"
-        toggle_cat
-      end
+      menu1(action)
     end
   end
 end
 
 # menu provisional para testear
 app = Categories.new
-app.menu_prov
+app
--- src/m_categories.rb
+++ src/m_categories.rb
@@ -1,2 +1,88 @@
 module Mcategories
+  def cat_get_info
+    category = print_get("Name: ")
+    while category == ""
+      puts "Cannot be blank"
+      category = print_get("Name: ")
+    end
+
+    transaction_type = print_get("Transaction type: ")
+    until ["income", "expense"].include?(transaction_type)
+      puts "Only income or expense"
+      transaction_type = print_get("Transaction type: ")
+    end
+    { name: category, transaction_type: transaction_type }
+  end
+
+  # new_transaction = { "amount": 2000,"notes": "TestNotes","date": "2020-10-10" }
+  def add_to_info
+    amount = print_get("Amount: ")
+    while amount == "" || !amount.to_i.positive?
+      puts "Cannot be zero"
+      amount = print_get("Amount: ")
+    end
+    date = print_get("Date: ")
+    while date == "" || (valid_date(date)) == false
+      puts "Required format: YYYY-MM-DD"
+      date = print_get("Date: ")
+    end
+    notes = print_get("Notes: ")
+    { amount: amount.to_i, notes: notes, date: date }
+  end
+
+  def print_get(prompt)
+    print prompt
+    gets.chomp
+  end
+
+  def valid_date(value)
+    y, m, d = value.split "-"
+    Date.valid_date? y.to_i, m.to_i, d.to_i
+  end
+
+  def id_validation(action_id)
+    id = get_id(action_id)
+    if id == "no"
+      [id.to_i, true]
+    else
+      [id.to_i, (@index_cat.select { |n| n[:id] == id.to_i }).empty?]
+    end
+  end
+
+  def get_id(data)
+    a = /^\w+-?\w+\s(\d+)$/
+    data.match(a).nil? ? "no" : data.match(a)[1]
+  end
+
+  def menu1(action)
+    if action == "create"
+      create_cat
+    elsif action == "show"
+      show_cat(@toggle_value)
+    elsif action == "next"
+      next_cat
+    elsif action == "prev"
+      preview_cat
+    elsif action == "logout"
+      @cat.logout
+    elsif action != ""
+      menu2(action)
+    else
+      puts "Invalid option"
+    end
+  end
+
+  def menu2(action)
+    if action.include? "update"
+      id_validation(action)[1] == false ? update_cat(id_validation(action)[0]) : (puts "Not Found")
+    elsif action.include? "delete"
+      id_validation(action)[1] == false ? delete_cat(id_validation(action)[0]) : (puts "Not Found")
+    elsif action.include? "add-to"
+      id_validation(action)[1] == false ? add_to_cat(id_validation(action)[0]) : (puts "Not Found")
+    elsif action == "toggle"
+      toggle_cat
+    else
+      puts "Invalid option"
+    end
+  end
 end
